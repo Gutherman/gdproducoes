eff2f9e907b3c5093244f183bac9694192222f84
Gutherman <99190558+Gutherman@users.noreply.github.com>
2023-04-26 21:32:20 -0300

    Update enviar_email.php

--- enviar_email.php
+++ enviar_email.php
@@ -1,52 +1,70 @@
 <?php
 
-require_once 'enviar_email\lib\vendor\autoload.php'; // Atualize com o caminho absoluto para o diretório onde o arquivo autoload.php foi gerado pelo Composer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-use enviar_email\lib\vendor\phpmailer\phpmailer\src\PHPMailer.php;
-use enviar_email\lib\vendor\phpmailer\phpmailer\src\Exception.php;
+require 'vendor/autoload.php';
 
-require 'enviar_email\lib\vendor\phpmailer\phpmailer\src\Exception.php';
-require 'enviar_email\lib\vendor\phpmailer\phpmailer\src\PHPMailer.php';
-require 'enviar_email\lib\vendor\phpmailer\phpmailer\src\SMTP.php';
-
-// Verificar se o formulário foi enviado
+// Verifica se o formulário foi submetido via POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $assunto = $_POST["assunto"];
     $nome = $_POST["nome"];
     $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $contatoAnterior = $_POST["contatoAnterior"];
     $mensagem = $_POST["mensagem"];
 
-    // Validar os campos do formulário
-    if (empty($nome) || empty($email) || empty($mensagem)) {
-        echo 'Por favor, preencha todos os campos do formulário.';
-        exit;
-    }
-
-    // Instanciar o objeto PHPMailer
-    $mail = new PHPMailer(true);
-
     try {
-        // Configurações do servidor de e-mail
+        // Configuração do servidor SMTP do Gmail
+        $smtpServer = 'smtp.gmail.com';
+        $smtpPort = 587;
+        $smtpUsuario = 'contatogdvideos@gmail.com';
+        $smtpSenha = 'Gm102030!';
+
+        // Cria uma nova instância do PHPMailer
+        $mail = new PHPMailer(true);
+
+        // Configura o servidor SMTP
         $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com'; // substitua pelo endereço do servidor SMTP
+        $mail->SMTPDebug = SMTP::DEBUG_OFF; // Altere para SMTP::DEBUG_SERVER para exibir mensagens de debug detalhadas
+        $mail->Host = $smtpServer;
+        $mail->Port = $smtpPort;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use PHPMailer::ENCRYPTION_SMTPS se o servidor SMTP requerer SSL
         $mail->SMTPAuth = true;
-        $mail->Username = 'contatogdvideos@gmail.com'; // substitua pelo seu e-mail
-        $mail->Password = 'Gm102030!'; // substitua pela sua senha
-        $mail->SMTPSecure = 'tls'; // ou 'ssl' se necessário
-        $mail->Port = 587; // ou outra porta utilizada pelo servidor SMTP
+        $mail->Username = $smtpUsuario;
+        $mail->Password = $smtpSenha;
 
-        // Configurações de envio de e-mail
-        $mail->setFrom('contatogdvideos@gmail.com', 'E-mails do Site'); // substitua pelo seu e-mail e nome
-        $mail->addAddress('contato@gdproducoes.com.br', 'Nome de cada cliente'); // substitua pelo e-mail e nome do destinatário
-        $mail->Subject = 'Assunto do E-mail';
-        $mail->Body = "Nome: $nome\nE-mail: $email\nMensagem: $mensagem";
+        // Cabeçalhos do email
+        $mail->setFrom($email, $nome);
+        $mail->addReplyTo($email);
+        $mail->addAddress('contato@gdproducoes.com.br'); // Endereço de email do destinatário
+        $mail->CharSet = 'UTF-8';
 
-        // Enviar o e-mail
-        $mail->send();
-
-        // Mensagem de confirmação
-        echo 'Obrigado por enviar essa mensagem! Retornaremos o mais breve possível.';
+        // Corpo do email
+        $mail->isHTML(true);
+        $mail->Subject = $assunto;
+        $mail->Body = "<p><strong>Assunto:</strong> " . $assunto . "</p>";
+        $mail->Body .= "<p><strong>Nome:</strong> " . $nome . "</p>";
+        $mail->Body .= "<p><strong>Email:</strong> " . $email . "</p>";
+        $mail->Body .= "<p><strong>Telefone:</strong> " . $telefone . "</p>";
+        $mail->Body .= "<p><strong>Contato Anterior na GD Produções:</strong> " . $contatoAnterior . "</p>";
+        $mail->Body .= "<p><strong>Mensagem:</strong> " . $mensagem . "</p>";
+                
+        // Anexos do email (opcional)
+        // $mail->addAttachment('/caminho/para/arquivo1.pdf', 'nome_do_arquivo1.pdf');
+        // $mail->addAttachment('/caminho/para/arquivo2.pdf', 'nome_do_arquivo2.pdf');
+        
+         // Tenta enviar o email
+        if ($mail->send()) {
+        echo "Mensagem enviada com sucesso!";
+        } else {
+        echo "Ocorreu um erro ao enviar a mensagem: " . $mail->ErrorInfo;
+}
     } catch (Exception $e) {
-        echo 'Erro ao enviar o e-mail: ' . $mail->ErrorInfo;
+        // Redireciona para a página inicial após 3 segundos
+        header("refresh:3;url=index.php"); // Substitua "index.php" pelo nome da sua página inicial
+        exit;
     }
 }
 ?>
